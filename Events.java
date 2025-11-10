dc56838ab941934cdb72a99ead4027dcb0e796e0
williamCao1991 <baocao922@gmail.com>
2025-11-10 10:44:47 -0500

    basic params

--- Events.java
+++ Events.java
@@ -2,5 +2,46 @@
  *
  * @author
  * Date   11/10/25
- * Purpose
+ * Purpose  A compass of Events desscriptions, dates, and functionality of "Wavelink"
+ *
  */
+import java.time.LocalTime;//imports the current time functions
+import java.time;
+import java.util.*;
+
+public class Events{
+    private String name;
+    private String description;
+    private String date;
+    private int people_assgn;
+    private int userOneCard;
+
+
+    public Events(String Name, String Description, String Date, int people_assgn, int UserOneCard){
+        this.name = Name;
+        this.date = Date;
+        this.description =Description;
+        this.userOneCard = UserOneCard;
+        this.people_assgn = people_assgn;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getPeople_assgn(){
+        return people_assgn;
+    }
+    public int getUserOneCard(){
+        return userOneCard;
+    }
+
+
+
+}

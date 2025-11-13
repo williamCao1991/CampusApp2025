5359c12b8bc3cc988e6c42e771592daebeb2876d
bcollins1503 <bcollins1503@gmail.com>
2025-11-13 10:17:46 -0500

    .

---
+++ Login_User.java
@@ -0,0 +1,54 @@
+/**
+ * @author
+ * Date: 11/10/2025
+ * Purpose:
+ */
+
+public class Login_User {
+
+    private String name;
+    private int userNum;
+
+    //constructor
+
+    /**
+     *
+     * @param name
+     * @param userNum
+     */
+    public Login_User(String name, int userNum){
+        this.name = name;
+        this.userNum userNum;
+    }
+
+    //getters
+
+    /**
+     *
+     * @return name
+     */
+    public String getName() {return name;}
+
+    /**
+     *
+     * @return userNum
+     */
+    public String getUserNum() {return userNum;}
+
+    //setters
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name){this.name = name;}
+
+    /**
+     *
+     * @param userNum
+     */
+    public void setUserNum(String userNum){this.userNum = userNum;}
+
+
+
+}
